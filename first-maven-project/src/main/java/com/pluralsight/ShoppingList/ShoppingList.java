273d8af994868ce3505769c58e0d1b233258f9d5
FIRST_NAME LAST_NAME <my_name@example.com>
2025-04-13 18:13:53 -0400

    init commit

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight.ShoppingList;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+// your code goes here
+   System.out.println("Egg, Chicken, Bread, Milk, Butter, Biscuit, Orange, Banana, pancake, juice");
+    }
+}
